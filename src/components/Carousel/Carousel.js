bf4323cd8037194e39ec680185ebd2fbc51d565b
Rekha <rekhamails260591@gmail.com>
2021-04-18 01:25:20 +0530

    Added Carousel with some data

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import data from '../../data';
+import './Carousel.css';
+
+const Carousel = () => {
+    
+    const [count, setCount ] = useState(3);
+    const [products, setProducts] = useState([...data.products]);
+
+    const nextSlide = () => {
+        console.log("count in next slide : ",count)
+        setProducts(data.products.slice(count, count+3));
+        setCount(count+3);
+    }
+
+    const prevSlide = () => {
+        console.log("count in prev slide", count, data.products);
+        setProducts(data.products.slice(count-6,count));
+        console.log(products);
+        setCount(count-3);
+    }
+
+    return (
+        <div className="carousel">
+            <p>{count} {data.products.length}</p>
+            <div className="carousel-container">
+                {products.map(product => {
+                    return (
+                        <div key={product._id} className="card">
+                            <img className="medium" src={product.image} alt={product.name}/>
+                            <div className="card-body">
+                                 <h2>{product.name}</h2>
+                                <div className="price">${product.price}</div>
+                            </div>
+                         </div>  
+                )
+
+                })
+                }
+            </div>
+            <ul className="carousel-controls">
+                <li id="chevron-arrow-left" className={count <= 3 ? 'disabled' : '' } onClick={prevSlide}></li>
+                <li id="chevron-arrow-right" className={count >= data.products.length ? 'disabled' : '' } onClick={nextSlide}></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Carousel;
---
+++ src/data.js
@@ -0,0 +1,89 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nile Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120
+        },
+        {
+            _id: '2',
+            name: 'Nike Slim Pant',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 78
+        },
+        {
+            _id: '3',
+            name: 'Adidas Fit Shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 100
+        },
+        {
+            _id: '4',
+            name: 'Puma Slim Pant',
+            category: 'Pants',
+            image: '/images/p5.jpg',
+            price: 65
+        },
+        {
+            _id: '5',
+            name: 'Adidas Sli Pant',
+            category: 'Pants',
+            image: '/images/p6.jpg',
+            price: 139
+        },
+        {
+            _id: '6',
+            name: 'Lacoste Free Shirt',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 220
+        },
+        {
+            _id: '11',
+            name: 'Nile Slim Shirt1',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120
+        },
+        {
+            _id: '12',
+            name: 'Nike Slim Pant1',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 78
+        },
+        {
+            _id: '13',
+            name: 'Adidas Fit Shirt1',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 100
+        },
+        {
+            _id: '14',
+            name: 'Puma Slim Pant1',
+            category: 'Pants',
+            image: '/images/p5.jpg',
+            price: 65
+        },
+        {
+            _id: '15',
+            name: 'Adidas Sli Pant1',
+            category: 'Pants',
+            image: '/images/p6.jpg',
+            price: 139
+        },
+        {
+            _id: '16',
+            name: 'Lacoste Free Shirt1',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 220
+        }
+    ]
+}
+export default data;
